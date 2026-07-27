27633e6e8a611c6de668b0e8a489f54dfd48945a
Jesse Anderson <jesse.c.anderson@icloud.com>
2026-07-27 11:46:47 -0400

    fix cask version interpolation

--- Casks/cyberdeck.rb
+++ Casks/cyberdeck.rb
@@ -2,7 +2,7 @@ cask "cyberdeck" do
   version "0.3.2"
   sha256 "0d0c4fcb2251390b4429f43e98186eed0ed08ded938bb6a0a7dff5735461c0b8"
 
-  url "https://github.com/jessecanderson/cyberdeck/releases/download/v0.3.2/cyberdeck-0.3.2-macos-arm64.tar.gz"
+  url "https://github.com/jessecanderson/cyberdeck/releases/download/v#{version}/cyberdeck-#{version}-macos-arm64.tar.gz"
   name "Cyberdeck"
   desc "Neon, keyboard-first TUI for running multiple local coding agents"
   homepage "https://github.com/jessecanderson/cyberdeck"
