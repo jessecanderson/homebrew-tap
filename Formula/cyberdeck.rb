0bb72469f8714f71fe6f99331eaefa881a9ca1d9
Jesse Anderson <jesse.c.anderson@icloud.com>
2026-07-27 13:43:48 -0400

    Declare standalone formula version (#12)
    * declare standalone formula version
    
    * order formula version stanza
    
    * fix: migrate formula version scheme

--- Formula/cyberdeck.rb
+++ Formula/cyberdeck.rb
@@ -2,8 +2,10 @@ class Cyberdeck < Formula
   desc "Neon, keyboard-first TUI for running multiple local coding agents"
   homepage "https://github.com/jessecanderson/cyberdeck"
   url "https://github.com/jessecanderson/cyberdeck/releases/download/v0.3.3/cyberdeck-0.3.3-macos-arm64.tar.gz"
+  version "0.3.3"
   sha256 "244dd23241605da5ed0b49cbab70c0de0d3807434fdcda0806bea5095b21aba4"
   license "Apache-2.0"
+  version_scheme 1
 
   depends_on arch: :arm64
   depends_on macos: :big_sur
